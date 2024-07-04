09446fec2a1727c20dbf8e642d1d6cfb764dbcef
eliseu441 <eliseu441@gmail.com>
2024-07-04 19:55:47 -0300

    fix build

--- front-hermes/next.config.mjs
+++ front-hermes/next.config.mjs
@@ -4,8 +4,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const nextConfig = {
-  distDir: 'build',
-  
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
       }
