a676b16b28184fe21a64c7e510c71602e152570a
eliseu441 <eliseu441@gmail.com>
2024-07-05 11:15:55 -0300

    vercel https

--- front-vite/src/api/Api.ts
+++ front-vite/src/api/Api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 //const baseURL = `http://127.0.0.1:8080/api/`
-//const baseURL = `http://hermes-vite-773ncdkkz-eliseu441s-projects.vercel.app/api/`
-
-const baseURL = `http://34.121.103.54/api/`
+const baseURL = `https://hermes-vite-eliseu441s-projects.vercel.app/api/`
+//const baseURL = `http://34.121.103.54/api/`
 
 export default axios.create({
 	baseURL,
